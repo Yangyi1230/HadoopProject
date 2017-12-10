d6ade2239316875a62f6d84d7d29f5ac8043baf7
Dayou Du <dudayou1901@gmail.com>
2017-12-10 01:11:41 -0500

    update 311Classifier

--- CombineAnalytic/311Classify/311Classify.scala
+++ CombineAnalytic/311Classify/311Classify.scala
@@ -3,14 +3,14 @@
 import org.apache.spark.mllib.clustering.{KMeans, KMeansModel}
 import org.apache.spark.mllib.linalg.Vectors
 
-val data = sc.textFile("SparkInput/crime.csv")
+val data = sc.textFile("311data/output2/part-r-*")
 
-val dataArray = data.map(s => s.split(','))
+val dataArray = data.map(s => s.split('\t'))
 
 val model = KMeansModel.load(sc, "/user/dd2645/KMeansModel") 
 
 case class Result (DataSourceId:Int, PredictionId:Int, Latitude:Double, Longtitude:Double, Score: Double)
 
-val res = dataArray.map(a => Result(1, model.predict(Vectors.dense(a(3).toDouble,a(4).toDouble)),a(3).toDouble,a(4).toDouble,1)).toDF() 
+val res = dataArray.map(a => Result(2, model.predict(Vectors.dense(a(3).toDouble,a(4).toDouble)),a(3).toDouble,a(4).toDouble,1)).toDF() 
 
-res.coalesce(1).write.format("com.databricks.spark.csv").option("header","false").save("SparkOutput/outCrime")
+res.coalesce(1).write.format("com.databricks.spark.csv").option("header","false").save("SparkOutput/out311")
